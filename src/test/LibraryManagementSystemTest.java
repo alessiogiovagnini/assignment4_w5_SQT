cb2f49b8053bad7cd23c722d20c47dda427fa20e
alessiogiovagnini <alessio.giovagnini@usi.ch>
2024-03-29 10:14:21 +0100

    added class for test

---
+++ src/test/LibraryManagementSystemTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class LibraryManagementSystemTest {
+
+    @Test
+    public void TestBookDamaged() {
+
+    }
+
+}
